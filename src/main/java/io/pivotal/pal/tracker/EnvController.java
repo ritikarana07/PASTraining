dbf83e7022bd4f33c4b574d002a409bc77b7bff0
Ubuntu <ubuntu@ip-172-31-18-0.eu-west-1.compute.internal>
2021-03-16 10:48:30 +0000

    lab2

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,12 @@
+package test.pivotal.pal.tracker;
+
+import java.util.Map;
+
+public class EnvController {
+    public EnvController(String port, String memory_limit, String cf_instance_index, String cf_instance_addr) {
+    }
+
+    public Map<String, String> getEnv() {
+        return null;
+    }
+}
