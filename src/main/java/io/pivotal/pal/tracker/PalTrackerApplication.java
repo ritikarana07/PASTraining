2b7a5e95b2c2aba33c069d96535ea690c838930a
Ubuntu <ubuntu@ip-172-31-18-0.eu-west-1.compute.internal>
2021-03-17 16:00:03 +0000

    jdbc

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -3,6 +3,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
@@ -12,8 +14,9 @@ public class PalTrackerApplication {
 
     }
     @Bean
-    public TimeEntryRepository timeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository timeEntryRepository(DataSource datasource ){
+        //return new InMemoryTimeEntryRepository();
+        return new JdbcTimeEntryRepository(datasource);
     }
 
 }
